e58da98aec0e76895a3660c3c2499d138f0406dc
Megha Singhania <megha@machobear.ca>
2022-04-01 13:37:53 -0700

    fix(contact-us): fix form validation

--- src/features/contact-us/form.css.ts
+++ src/features/contact-us/form.css.ts
@@ -1,5 +1,94 @@
-import { style } from '@vanilla-extract/css'
+import { style, globalStyle } from '@vanilla-extract/css'
+import { formVars, vars } from '@/theme.css'
 
 export const formContainer = style({
   padding: 0
+})
+
+export const wrapper = style({
+  position: 'relative',
+  width: '100%',
+  marginBottom: '25px'
+})
+
+export const input = style({
+  display: 'block',
+  width: '100%',
+  maxWidth: '100%',
+  height: 24,
+  color: formVars.text.value,
+  fontSize: vars.fontSizes.medium,
+  padding: 0,
+  borderRadius: 'none',
+  border: 'none',
+  borderBottom: `1px solid ${formVars.base.default}`,
+  outline: 'none',
+  cursor: 'text',
+
+  ':focus': {
+    borderBottom: `1px solid ${formVars.base.focus}`
+  },
+
+  ':disabled': {
+    borderBottom: `1px solid ${formVars.base.disabled}`
+  },
+
+  ':invalid': {
+    borderBottom: `1px solid ${formVars.base.error}`
+  }
+})
+
+export const label = style({
+  fontFamily: vars.fonts.body,
+  fontSize: vars.fontSizes.xSmall,
+  lineHeight: vars.fontSizes.xSmall,
+  color: vars.colors.neutral50
+})
+
+export const buttonBase = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: 'fit-content',
+  boxSizing: 'border-box',
+  borderRadius: '24px',
+  padding: '8px 32px',
+  textTransform: 'uppercase',
+  fontFamily: vars.fonts.heading,
+  color: vars.colors.pureWhite,
+  background: vars.colors.neutral80,
+  border: `1px solid ${vars.colors.neutral80}`,
+  margin: 0,
+
+  ':hover': {
+    color: vars.colors.black,
+    background: vars.colors.pureWhite,
+    border: `1px solid ${vars.colors.black}`
+  },
+
+  ':disabled': {
+    color: vars.colors.neutral5,
+    background: vars.colors.neutral50,
+    border: `1px solid ${vars.colors.neutral50}`
+  }
+})
+
+globalStyle(`${input}:placeholder-shown + label`, {
+  visibility: 'hidden',
+  zIndex: -1,
+  transition: ' 0.2s ease-in-out'
+})
+
+globalStyle(`${input}:not(:placeholder-shown) + label`, {
+  visibility: 'visible',
+  opacity: 1,
+  transform: 'translateY(-40px)',
+  transition: '0.2s ease-in-out transform'
+})
+
+globalStyle(`${input}:focus:not(:placeholder-shown) + label`, {
+  visibility: 'visible',
+  opacity: 1,
+  transform: 'translateY(-40px)',
+  transition: '0.2s ease-in-out transform'
 })
--- src/features/contact-us/form.tsx
+++ src/features/contact-us/form.tsx
@@ -1,116 +1,103 @@
 import classNames from 'classnames'
-import { formContainer } from './form.css'
+import { formContainer, input, wrapper, label, buttonBase } from './form.css'
 
 const ContactUsForm = (): JSX.Element => {
   return (
-    <div id="mc_embed_signup">
-      <form
-        action="https://machobear.us14.list-manage.com/subscribe/post?u=eb9f58a9f9f65c3f093576ce0&amp;id=9c2c2f819b"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        className={classNames('validate', formContainer)}
-        target="_blank"
-        noValidate
-      >
-        <div id="mc_embed_signup_scroll">
-          <div className="mc-field-group">
-            <label htmlFor="mce-EMAIL">
-              Email Address <span className="asterisk">*</span>
-            </label>
-            <input
-              type="email"
-              name="EMAIL"
-              className="required email"
-              id="mce-EMAIL"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-FULLNAME">
-              Full Name <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="FULLNAME"
-              className="required"
-              id="mce-FULLNAME"
-            />
-          </div>
-          <div className="mc-field-group size1of2">
-            <label htmlFor="mce-PHONE">
-              Phone Number <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="PHONE"
-              className="required"
-              id="mce-PHONE"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-JOBTITLE">
-              Job Title <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="JOBTITLE"
-              className="required"
-              id="mce-JOBTITLE"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-RCOUNT">
-              Restaurant Count <span className="asterisk">*</span>
-            </label>
-            <select name="RCOUNT" className="required" id="mce-RCOUNT">
-              {/* <option value=""></option> */}
-              <option value="0-2">0-2</option>
-              <option value="3-9">3-9</option>
-              <option value="10+">10+</option>
-            </select>
-          </div>
-          <div className="mc-field-group">
-            <label htmlFor="mce-BNAME">Business Name</label>
-            <input type="text" name="BNAME" className="" id="mce-BNAME" />
-          </div>
-          <div hidden>
-            <input type="hidden" name="tags" value="7232977" />
-          </div>
-          <div id="mce-responses" className="clear">
-            <div
-              className="response"
-              id="mce-error-response"
-              style={{ display: 'none' }}
-            ></div>
-            <div
-              className="response"
-              id="mce-success-response"
-              style={{ display: 'none' }}
-            ></div>
-          </div>
-          {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-          <div
-            style={{ position: 'absolute', left: '-5000px' }}
-            aria-hidden="true"
-          >
-            <input
-              type="text"
-              name="b_eb9f58a9f9f65c3f093576ce0_9c2c2f819b"
-              tabIndex={-1}
-            />
-          </div>
-          <div className="clear">
-            <input
-              type="submit"
-              value="Submit"
-              name="Submit"
-              id="mc-embedded-subscribe"
-              className="button"
-            />
-          </div>
-        </div>
-      </form>
-    </div>
+    <form
+      action="https://machobear.us14.list-manage.com/subscribe/post?u=eb9f58a9f9f65c3f093576ce0&amp;id=9c2c2f819b"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      className={classNames('validate', formContainer)}
+      target="_blank"
+    >
+      <div className={wrapper}>
+        <label htmlFor="mce-EMAIL" className={label}>
+          Email Address <span className="asterisk">*</span>
+        </label>
+        <input
+          type="email"
+          name="EMAIL"
+          className={classNames('email', 'required', input)}
+          id="mce-EMAIL"
+          required
+        />
+      </div>
+      <div className={wrapper}>
+        <label htmlFor="mce-FULLNAME" className={label}>
+          Full Name <span className="asterisk">*</span>
+        </label>
+        <input
+          type="text"
+          name="FULLNAME"
+          className={classNames('required', input)}
+          id="mce-FULLNAME"
+          required
+        />
+      </div>
+      <div className={wrapper}>
+        <label htmlFor="mce-PHONE" className={label}>
+          Phone Number <span className="asterisk">*</span>
+        </label>
+        <input
+          type="text"
+          name="PHONE"
+          className={classNames('required', input)}
+          id="mce-PHONE"
+          required
+        />
+      </div>
+      <div className={wrapper}>
+        <label htmlFor="mce-JOBTITLE" className={label}>
+          Job Title <span className="asterisk">*</span>
+        </label>
+        <input
+          type="text"
+          name="JOBTITLE"
+          className={classNames('required', input)}
+          id="mce-JOBTITLE"
+          required
+        />
+      </div>
+      <div className={wrapper}>
+        <label htmlFor="mce-RCOUNT" className={label}>
+          Restaurant Count <span className="asterisk">*</span>
+        </label>
+        <select
+          name="RCOUNT"
+          className={classNames('required', input)}
+          id="mce-RCOUNT"
+        >
+          <option value="0-2">0-2</option>
+          <option value="3-9">3-9</option>
+          <option value="10+">10+</option>
+        </select>
+      </div>
+      <div className={wrapper}>
+        <label htmlFor="mce-BNAME" className={label}>
+          Business Name
+        </label>
+        <input type="text" name="BNAME" className={input} id="mce-BNAME" />
+      </div>
+      <div hidden>
+        <input type="hidden" name="tags" value="7232977" />
+      </div>
+      {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+      <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+        <input
+          type="text"
+          name="b_eb9f58a9f9f65c3f093576ce0_9c2c2f819b"
+          tabIndex={-1}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Submit"
+        name="Submit"
+        id="mc-embedded-subscribe"
+        className={buttonBase}
+      />
+    </form>
   )
 }
 
