deb84c1096a18ac420796a2d30fef13ef1628770
Megha Singhania <megha@machobear.ca>
2022-04-08 16:42:08 -0700

    fix(home): add hero animation

--- src/features/home/hero.css.ts
+++ src/features/home/hero.css.ts
@@ -1,5 +1,21 @@
 import { vars } from '@/theme.css'
-import { style } from '@vanilla-extract/css'
+import { keyframes, style } from '@vanilla-extract/css'
+
+const appear = keyframes({
+  '0%': {
+    opacity: 0,
+    transform: 'translate(100px, -50%)'
+  },
+
+  '75%': {
+    opacity: 0.6,
+    transform: 'translate(0px, -50%)'
+  },
+
+  '100%': {
+    opacity: 1
+  }
+})
 
 export const heroTextWrapper = style({
   position: 'absolute',
@@ -9,6 +25,7 @@ export const heroTextWrapper = style({
   padding: '0px 32px',
   maxWidth: '720px',
   zIndex: vars.zIndex.hero,
+  animation: `${appear} 1s`,
 
   '@media': {
     'screen and (max-width: 767px)': {
