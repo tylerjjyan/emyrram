68142390b42233edb6f36713629edee687f522cd
Megha Singhania <megha@machobear.ca>
2022-04-13 10:18:17 -0700

    fix(privacy): update address

--- src/features/privacy-policy/index.tsx
+++ src/features/privacy-policy/index.tsx
@@ -316,7 +316,7 @@ const PRIVACY_CONTENT = [
         </Paragraph>
         <Paragraph
           variant="medium"
-          text={`900-1095 West Pender St, Vancouver, BC, V6E 2M6, Canada`}
+          text={`988 Expo Blvd, Vancouver, BC, V67 0G2, Canada`}
         />
       </>
     )
